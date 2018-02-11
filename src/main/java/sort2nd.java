9aa68cef19385d8733c173f6ad338f9794ca2d54
minemine <yang.zho@husku.neu.edu>
2018-02-11 15:27:59 -0500

    small bug exists

--- src/main/java/sort2nd.java
+++ src/main/java/sort2nd.java
@@ -10,11 +10,27 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.*;
 import java.io.*;
 
-class staYear implements WritableComparable<staYear>{
+import org.apache.hadoop.mapreduce.Partitioner;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+class staYear implements Writable, WritableComparable<staYear> {
     Text station;
     int year;
     public staYear(){}
-    public void write(DataOutput out) throws IOException{
+    public void write(DataOutput out) throws IOException {
         station.write(out);
         out.writeInt(year);
     }
@@ -32,6 +48,25 @@ class staYear implements WritableComparable<staYear>{
 
 }//define a cutom key (station, year)
 
+class info implements Writable {
+    int Count_max; float Sum_max;
+    int Count_min; float Sum_min;
+    int year;
+    public info() {
+
+    }
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(Count_max);out.writeFloat(Sum_max);
+        out.writeInt(Count_min);out.writeFloat(Sum_min);
+        out.writeInt(year);
+    }
+    public void readFields(DataInput in) throws IOException {
+        Count_max = in.readInt();Sum_max = in.readFloat();
+        Count_min = in.readInt();Sum_min = in.readFloat();
+        year=in.readInt();
+
+    }
+}//define a custom value
 public class sort2nd {
 
     public static int compare(int a,int b){
@@ -39,8 +74,8 @@ public class sort2nd {
         else if(a==b) {return 0;}
         else{return 1;}
     }
-    public static class groupComparator extends WritableComparable {
-        protected groupComparator(){
+    public static class groupComparator extends WritableComparator {
+        public groupComparator(){
             super(staYear.class, true);
         }
         public int compare(WritableComparable w1, WritableComparable w2){
@@ -49,8 +84,106 @@ public class sort2nd {
             return sy1.station.compareTo(sy2.station);
         }
     }// group comparator only compare station
+    public static class myMapper
+            extends Mapper<Object, Text, staYear, info>{
+        private  staYear sy = new staYear();
+        private  info pair = new info();
+
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String line = value.toString();
+            List<String> line2=new ArrayList<String>(Arrays.asList(line.split(",")));
+            pair.Count_max=0;pair.Sum_max=0;pair.Count_min=0;pair.Sum_min=0;//reset for method
+            if( line2.get(2).equals("TMAX") ) {
+                int year=Integer.parseInt(line2.get(1).substring(0,4));
+                sy.station=new Text(line2.get(0)); sy.year=year;
+                pair.Count_max+=1; pair.Sum_max+=Float.parseFloat(line2.get(3));pair.year=year;
+                context.write(sy,pair);
+            }
+            else if (line2.get(2).equals("TMIN") ) {
+                int year=Integer.parseInt(line2.get(1).substring(0,4));
+                sy.station=new Text(line2.get(0)); sy.year=year;
+                pair.Count_min+=1;pair.Sum_min+=Float.parseFloat(line2.get(3));
+                context.write(sy,pair);}
+            else {}
+
+        }
+
+    }
+
+    public static class myPartitioner
+            extends Partitioner<staYear, info> { //partition(hash) by station(string)
+        public int getPartition(staYear key, info value, int numPartitions) {
+            return Math.abs(key.station.toString().hashCode()) % numPartitions;
+        }
+    }
+
+    public static class myReducer
+            extends Reducer<staYear,info,Text,Text> {
+        private Text result = new Text();
+
+        public void reduce(staYear key, Iterable<info> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int count_max=0;int count_min = 0;
+            float sum_max=0;float sum_min=0;  //int i=0;
+            //for (info val : values){size+=1;}
+            Iterator<info> itr=values.iterator();
+            Iterator<info> itr2=values.iterator();
+            String maxString, minString; float maxMean,minMean;
+            String all=""; //to contain whole-station information
+            itr2.next(); info k; //itr2 is one element ahead of utr
+            while (itr.hasNext()) {
+                k=itr.next();
+                if(itr.hasNext()) {//if not the last element
+                  if(k.year!=itr2.next().year){ //itr2 next
+                      if (count_max==0){maxString="this station has no tmax record";}
+                      else{ maxMean=sum_max/count_max; maxString=Float.toString(maxMean);}
+                      if (count_min==0){minString="this station has no tmin record";}
+                      else{ minMean=sum_min/count_min; minString=Float.toString(minMean);}
+                      all=all+"year"+Integer.toString(k.year)+", "+minString+", "+maxString+"  ";
+                      count_max=0; count_min = 0;sum_max=0; sum_min=0;
+                  }
+                  else{
+                      count_max+=k.Count_max; sum_max+=k.Sum_max;
+                      count_min+=k.Count_min; sum_min+=k.Sum_min;
+                  }
+                }
+                else{ //the last element
+                    if (count_max==0){maxString="this station has no tmax record";}
+                    else{ maxMean=sum_max/count_max; maxString=Float.toString(maxMean);}
+                    if (count_min==0){minString="this station has no tmin record";}
+                    else{ minMean=sum_min/count_min; minString=Float.toString(minMean);}
+                    all=all+"year"+Integer.toString(k.year)+", "+minString+", "+maxString+"  ";
+                }
+            }
+            context.write(new Text(key.station) , new Text(all));
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "myjob");
+        job.setJarByClass(sort2nd.class);
+
+        job.setMapperClass(myMapper.class);
+        job.setReducerClass(myReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setMapOutputKeyClass(staYear.class);
+        job.setMapOutputValueClass(info.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+
+
 
 }
+
 
 
 
